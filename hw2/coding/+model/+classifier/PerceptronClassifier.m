e5d6e5cf5739a187c1926ef03fc666ead9b4d6d4
Smallmountain <sam123456777@gmail.com>
2014-10-22 21:30:45 +0800

    Complete perceptron classifier of hw2

--- hw2/coding/+model/+classifier/PerceptronClassifier.m
+++ hw2/coding/+model/+classifier/PerceptronClassifier.m
@@ -1,21 +1,60 @@
 
 classdef PerceptronClassifier < handle
-   properties
-      
-   end
+    
+    properties
+        level; % lifting level
+        param; % h is the parameters of hyperplane
+    end
    
-   methods
-       function classifier = PerceptronClassifier ()  % constructor
-           
-       end
-       function predictedValue = predict (obj, X)
-           
-       end
-   end
+    methods
+        function classifier = PerceptronClassifier (level, param)  % constructor
+            classifier.level = level;
+            classifier.param = param;
+        end
+        
+        function predictedValue = predict (obj, X)
+            % lifting
+            phiX = ones(num, obj.level + 1);
+            for i = 1 : obj.level
+                phiX(:, i+1) = X.^i;
+            end
+            
+            % predict
+            predictedValue = sign(phiX * obj.param);
+        end
+    end
    
-   methods (Static)
-      function classifier = train (X, y, liftingLevel)
-      	classifier = model.classifier.PerceptronClassifier();
-      end
-   end
+    methods (Static)
+        function classifier = train (X, y, liftingLevel)
+            % initializing variables
+            num = size(X, 1);
+            
+            % lifting
+            phiX = ones(num, liftingLevel + 1);
+            for i = 1 : liftingLevel
+                phiX(:, i+1) = X.^i;
+            end
+            
+            % adjustable variables
+            learningRate = 0.01;
+            epi = 0.000001;
+            
+            % initialize variables for iteration
+            %h = [1; 0.9487; -3.3650]; % update this
+            h = ones(liftingLevel + 1, 1);
+            updateVector = ones(liftingLevel + 1, 1);
+            
+            % converage
+            while norm(updateVector) > epi
+                % predict
+                predicted = sign(phiX * h);
+
+                % update h
+                updateVector = learningRate * (phiX' * (y - predicted));
+                h = h + updateVector;
+            end
+            
+            classifier = model.classifier.PerceptronClassifier(liftingLevel, h);
+        end
+    end
 end
