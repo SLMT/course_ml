ee2dc6da277bb09f22b8d65927b25b7577e05d1f
slmt <sam123456777@gmail.com>
2015-01-26 23:26:11 +0800

    Super powerful classifier

--- FinalProject/coding/+model/+classify/@MLFinalClassifier/MLFinalClassifier.m
+++ FinalProject/coding/+model/+classify/@MLFinalClassifier/MLFinalClassifier.m
@@ -1,54 +1,45 @@
 classdef MLFinalClassifier
     
     properties
-        training_normalized_X;
-		training_LDA_W;
+        training_X;
 		training_y;
 		training_n;
-		training_X_Mean;
-		training_X_Std;
         
         % Hyperparameters
-		gamma_K = 10;
-		gamma_S = 10;
-		lambda = 1;
+		gamma_K = 100;
+		gamma_S = 100;
+		lambda = 5;
 		miu = 1;
     end
     
     methods
         function obj = MLFinalClassifier(X, y)
+            obj.training_X = X;
             obj.training_y = y;
 			obj.training_n = size(X,1);
-            
-            % Z-Normalization
-            Xmean = mean(X);
-			Xstd = std(X);
-			obj.training_normalized_X = model.classify.MLFinalClassifier.zNormalize( X, Xmean, Xstd );
-            obj.training_X_Mean = Xmean;
-            obj.training_X_Std = Xstd;
-            
-            % get labeled data
-            labeled_x = obj.training_normalized_X(y ~= 0, :);
-            labeled_y = y(y ~= 0);
-
-            % LDA
-            obj.training_LDA_W = LDA(labeled_x, labeled_y);
 		end
         
         function y = predict(obj, X)
             % Define some constants
             predict_n = size(X, 1);
-            
-			% Normalize testing data ( Z-Normalization )
-			X = model.classify.MLFinalClassifier.zNormalize( X, obj.training_X_Mean, obj.training_X_Std );
-			
+
             % CAT X behind obj.training_X
-            longX = [ obj.training_normalized_X ; X ];
+            longX = [ obj.training_X ; X ];
             longY = [ obj.training_y; zeros(predict_n,1) ];
             long_n = obj.training_n + predict_n;
             
-            % Mapping to LDA_w
-            longX = [ ones(long_n, 1), longX] * obj.training_LDA_W';
+            % Z-Normalization
+            longXmean = mean(longX);
+			longXstd = std(longX);
+            longX = model.classify.MLFinalClassifier.zNormalize( longX, longXmean, longXstd );
+            
+            % get labeled data
+            labeled_x = longX(longY ~= 0, :);
+            labeled_y = longY(longY ~= 0);
+
+            % LDA
+            w = LDA(labeled_x, labeled_y);
+            longX = [ ones(long_n, 1), longX] * w';
             
             % Compute K (Gaussian Kernel)
             K = model.classify.MLFinalClassifier.getGaussianKernel( longX, obj.gamma_K );
